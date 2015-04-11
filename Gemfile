1a3732f4dacc24f6b2226d35d747a269fd24e712
akiyama <akiyama@lictoor.com>
2015-04-11 17:26:11 +0000

    Add a full_title helper

--- Gemfile
+++ Gemfile
@@ -6,6 +6,11 @@ gem 'mysql2'
 group :development, :test do
 # Use mysql as the database for Active Record
 gem 'rspec-rails', '2.13.1'
+gem 'guard', '2.6.1'
+gem 'guard-rspec', '2.5.0'
+gem 'spork-rails', '4.0.0'
+gem 'guard-spork', '1.5.0'
+gem 'childprocess', '0.3.6'
 end
 
 group :test do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "StaticPages" do
 
   describe "Home Page" do
-    it "works! 'test_app'" do
+    it "works! test_app" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
       expect(page).to have_content('test_app')
@@ -11,9 +11,13 @@ describe "StaticPages" do
     it "works! 'test_app'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      expect(page).to have_title('Akiyama | Home')
+      expect(page).to have_title("Akiyama")
     end
-  end
+    it "test_app" do
+	visit '/static_pages/home'
+	expect(page).not_to have_title('| Home')
+	end
+	end
 
 describe "HelpPage" do
     it "works! 'Help'" do
@@ -21,11 +25,15 @@ describe "HelpPage" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
-    it "works! 'Help'" do
+    it "works! Help" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/help'
-      expect(page).to have_title('Akiyama | Help')
+      expect(page).to have_title("Akiyama")
     end
+    it "Help" do
+	visit '/static_pages/help'
+	expect(page).not_to have_title('| Help')
+	end
   end
 
 describe "AboutPage" do
@@ -34,10 +42,32 @@ describe "AboutPage" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
-    it "works! 'About Us'" do
+    it "works! About Us" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/about'
-      expect(page).to have_title('Akiyama | About Us')
+      expect(page).to have_title("Akiyama")
     end
+    it "About Us" do
+	visit '/static_pages/about'
+	expect(page).not_to have_title('| About Us')
+	end
   end
+
+describe "ContactPage" do
+    it "works! 'Contact'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    it "works! Contact" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/contact'
+      expect(page).to have_title("Akiyama")
+    end
+    it "Contact" do
+	visit '/static_pages/contact'
+	expect(page).not_to have_title('| Contact')
+	end
+  end
+
 end
